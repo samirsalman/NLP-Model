6680e404e45b4284dc14c70d99de10736462f4dc
samirsalman <33979978+samirsalman@users.noreply.github.com>
2020-03-03 21:36:40 +0100

    fix names

--- clustering/word_embedding/main.py
+++ clustering/word_embedding/main.py
@@ -3,7 +3,6 @@ import json
 import numpy
 from gensim.models import Word2Vec
 from gensim.models.keyedvectors import Word2VecKeyedVectors
-from gingerit.gingerit import GingerIt
 
 model = Word2Vec.load('./wiki_iter=5_algorithm=skipgram_window=10_size=300_neg-samples=10.m')
 
@@ -15,34 +14,35 @@ def phrase2vec(s):
               vector = vector + model.wv[s[i]]
        return vector/len(s)
 
-def phrase_similarity(a,b):
+def vectors_similarity(a,b):
        cosine_similarity = numpy.dot(a, b) / (numpy.linalg.norm(a) * numpy.linalg.norm(b))
-       print(cosine_similarity)
+       return cosine_similarity
 
-def clean_corpus(s):
-       #TODO verificare se un articolo è maschile o femminile e cambiare l' con lo/la
-       stringa = re.sub(r'[^\w]', ' ', s)
-       stringa=stringa.replace("è", "essere")
-       stringa=stringa.replace("sarà", "diventa")
-       stringa=stringa.replace("morivazione", "motivazione")
-       return stringa.lower()
+def clean_phrases(s):
+       s = re.sub(r'[^\w]', ' ', s)
+       s =s.replace("è", "essere")
+       s = s.replace("sarà", "diventa")
+       s =s.replace("morivazione", "motivazione")
+       return s.lower()
 
 
-def carica_frase(path):
+def load_phrases(path):
        i = 0
        with open(path, 'r') as dataset:
               corpusJSON = json.load(dataset)
               for el in range(len(corpusJSON)):
                      try:
-                            frase_1= clean_corpus(corpusJSON[el]["messaggio"])
-                            frase_2 = clean_corpus(corpusJSON[el+1]["messaggio"])
-                            print(frase_1, frase_2)
-                            phrase_similarity(phrase2vec(frase_1), phrase2vec(frase_2))
+                            f1 = clean_phrases(corpusJSON[el]["messaggio"])
+                            f2 = clean_phrases(corpusJSON[el+1]["messaggio"])
+                            print(f1, f2)
+                            vectors_similarity(phrase2vec(f1), phrase2vec(f2))
                      except Exception as e:
-                            i+=1
+                            i += 1
                             print(e)
-       print(i)
-carica_frase("./dataset.json")
+       print("Number of errors: ", i)
+
+
+load_phrases("./dataset.json")
 
 
 
