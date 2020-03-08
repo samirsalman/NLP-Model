e631063e59a825943c4515a4ee53c2083fbb31db
samirsalman <33979978+samirsalman@users.noreply.github.com>
2020-03-08 21:13:40 +0100

    toJson method

--- clustering/word_embedding/main.py
+++ clustering/word_embedding/main.py
@@ -11,6 +11,7 @@ from matplotlib import pyplot as plt
 model = Word2Vec.load('./wiki_iter=5_algorithm=skipgram_window=10_size=300_neg-samples=10.m')
 global multi_dim
 global phrases
+global date
 
 def phrase2vec(s):
        s = s.split(" ")
@@ -69,11 +70,14 @@ def load_phrases(path):
               corpusJSON = json.load(dataset)
               for el in range(len(corpusJSON)):
                      if(el==0):
+                            global date
                             first = corpusJSON[el]["data"]
+                            date = first
                      if(el!=0):
                             if corpusJSON[el]["data"] != first:
+                                   bar.finish()
                                    return
-                     if el/len(corpusJSON)%8 == 0:
+                     if el%10==0:
                             bar.update(i + 1)
                      try:
                             print(first)
@@ -86,6 +90,7 @@ def load_phrases(path):
                             sim = vectors_similarity(v1, v2)
                             results.write(str(f1) + "," + str(f2) + "," + str(v1) + ","+ str(v2) + ","+str(sim) + "\n")
                             dataset_output.write(str(f1) + "," + str(v1).rstrip() + "\n")
+
                      except Exception as e:
                             i += 1
                             print(e)
@@ -95,8 +100,28 @@ def load_phrases(path):
        results.close()
 
 
+def write_json(k_means, centroids):
+       results = {}
+       global phrases
+       results["id"] = date
+       results["cendroids"] = []
+       results["elements"] = []
+
+       for i in range(len(k_means.cluster_centers_)):
+              print(i)
+              results["cendroids"].append({"cluster": i, "phrase": str(phrases[centroids[i]])})
+
+       for i in range(len(k_means.labels_)):
+              results["elements"].append({"phrase": phrases[i], "cluster": k_means.labels_[i]})
+
+       file = open("./clusters_results.json", "w+")
+       to_json = json.dumps(results)
+       file.write(to_json)
+
+
 def make_clusters(K):
        load_phrases("./dataset.json")
+       indxes_of_centroid = []
        X = []
        for el in multi_dim:
               X.append(np.resize(el,2))
@@ -115,17 +140,12 @@ def make_clusters(K):
            c='red', edgecolor='black',
            label='centroids')
        plt.show()
+       test = {i: np.where(k_means.labels_ == i)[0] for i in range(k_means.n_clusters)}
+       for i in test.keys():
+              indxes_of_centroid.append(test[i][0])
+       print(indxes_of_centroid)
 
-       results = []
-       global phrases
-       k = 0
-       for i in k_means.labels_:
-              results.append([i, phrases[k]])
-              k+=1
-
-       file = open("./clusters_results.txt","w+")
-       file.write(str(results))
-       print(results)
+       write_json(k_means,indxes_of_centroid)
 
 
 make_clusters(3)
