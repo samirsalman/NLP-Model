6d8a63aca39722639e666ceb4e60932ac7c437f1
LeonardoE95 <leonardotamiano95@gmail.com>
2020-03-08 18:03:57 +0100

    Back_end/ Aggiunto supporto per utilizzo delle APIs di google drive al fine di scaricare il dataset.

--- back_end/google_drive_api/index.js
+++ back_end/google_drive_api/index.js
@@ -1,4 +1,3 @@
-// ----- Google drive -----
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
@@ -8,7 +7,14 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'google_drive_api/token.json';
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Drive API.
+  authorize(JSON.parse(content), listFiles);
+});
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -60,29 +66,25 @@ function getAccessToken(oAuth2Client, callback) {
   });
 }
 
-// Load client secrets from a local file.
-fs.readFile('google_drive_api/credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Drive API.
-
-  authorize(JSON.parse(content), processFiles);
-});
-
-function processFiles(auth)
-{
+/**
+ * Lists the names and IDs of up to 10 files.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listFiles(auth) {
   const drive = google.drive({version: 'v3', auth});
-
-  // TODO: Download available files
+  drive.files.list({
+    pageSize: 10,
+    fields: 'nextPageToken, files(id, name)',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const files = res.data.files;
+    if (files.length) {
+      console.log('Files:');
+      files.map((file) => {
+        console.log(`${file.name} (${file.id})`);
+      });
+    } else {
+      console.log('No files found.');
+    }
+  });
 }
-
-// ----- RESTful web interface  -----
-
-const express = require('express');
-const app = express();
-
-app.get("/", function(req, res) {
-    res.end("Hello World!")
-})
-
-// Listen using PORT env variable, or 3000 if not defined
-app.listen(process.env.PORT || 3000);
